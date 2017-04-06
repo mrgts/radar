045bb6abdf50865336a10469cd85a562e4d292e4
mrgts <oscarfelipe2506@gmail.com>
2017-04-05 23:49:55 -0500

    Update TEST_FINAL.ino

--- TEST_FINAL/TEST_FINAL.ino
+++ TEST_FINAL/TEST_FINAL.ino
@@ -4,60 +4,58 @@ Autor: oscar palacios
 Fecha: 15/04/2013
  Arduino SRF05 Radar Sketch
 */
-#include <Servo.h>    // Añade la libreria Servo
-Servo leftRightServo;    // Crea una variable servo
-int leftRightPos = 0;    // Variable para la posición del servo (grados)
-int index = 0;    // Contador para el número de lectura
-long total = 0;    // Variable para calcular el pormedio
-int average = 0;    // Variable para calcular el promedio
-long duration = 0;    // Variable para calcular el tiempo de llegada de la señal
-int distance = 0; // Variable para calcular la distancia
-// CONFIGURACIÓN:
-int echoPin = 11;    // SRF05's Echo pin
-int trigPin = 10;    // SRF05's Trig pin
-int servoPin = 12;    // Pin para el cable de señal del servo
-int servoMin = 700;    // Anchura del pulso, en microsegundos, correspondiente al mínimo (0 grados) ángulo en del servo (por defecto 544)
-int servoMax = 2400;    // Anchura del pulso, en microsegundos, correspondiente al máximo (180 grados) ángulo en del servo (por defecto 2400)
-const int numReadings = 2;    // Número de lecturas en cada posición
+#include <Servo.h>   
+Servo leftRightServo;   
+int leftRightPos = 0;    
+int index = 0;  
+long total = 0;    
+int average = 0;    
+long duration = 0;  
+int distance = 0; 
+// CONFIGURACIÓN
+int echoPin = 11;   
+int trigPin = 10;    
+int servoPin = 12;    
+int servoMin = 700;    
+int servoMax = 2400;    
+const int numReadings = 2;    
 void setup() {
-leftRightServo.attach(servoPin,700,2400);    // Pin de salida para el servo, recorrido minimo, recorrido maximo
-Serial.begin(9600);    // Establece la velocidad de datos del puerto serie
-pinMode(trigPin, OUTPUT);    // Establece pin como salida
-pinMode(echoPin, INPUT);    // Establece pin como entrada
-digitalWrite(trigPin, LOW);    // Pone el pin a un estado logico bajo
+leftRightServo.attach(servoPin,700,2400);    
+Serial.begin(9600);   
+pinMode(trigPin, OUTPUT);    
+pinMode(echoPin, INPUT);   
+digitalWrite(trigPin, LOW);   
 } 
   
 void loop() {  
-    for(leftRightPos = 0; leftRightPos < 180; leftRightPos++) {    // De izquierda a derecha.
+    for(leftRightPos = 0; leftRightPos < 180; leftRightPos++) {  
     leftRightServo.write(leftRightPos);
-        for (index = 0; index<numReadings; index++) {    // Repite tantas veces como número de lecturas en cada posicion           
-        // Pulso de 10us para inicial el modulo
+        for (index = 0; index<numReadings; index++) {   
         digitalWrite(trigPin, HIGH); 
         delayMicroseconds(10); 
         digitalWrite(trigPin, LOW);
      
-        duration = pulseIn(echoPin, HIGH, 17400);    // Devuelve la longitud del pulso del pin Echo en us (3metros maximo)
-            if (!duration){    // Si la duracción es 0
-            duration = 17400;    // La señal se a perdido. Se establece distancia maxima
+        duration = pulseIn(echoPin, HIGH, 17400);    
+            if (!duration){    
+            duration = 17400;    
             }
-        distance = duration/58;    // Calculamos distancia en centrimetros
-        total = total + distance;    // Almacenamos la distancia para calcular promedio
-        delay(50);    // Esperamos hasta la siguiente medida
-        }
+        distance = duration/58;    
+        total = total + distance;    
+        delay(50);   
      
-    average = total/numReadings;    // Calcula el promedio
-    total = 0;    // Resetea variable
+    average = total/numReadings;    
+    total = 0;    
    
-    // Envia datos por el puerto serie
-    Serial.print("X");    // Identificador X para la posicion del servo
-    Serial.print(leftRightPos);    // Posición del servo
-    Serial.print("V");    // Identificador V para el promedio de distancia
-    Serial.println(average);    // Promedio de distancia
+    
+    Serial.print("X");   
+    Serial.print(leftRightPos);   
+    Serial.print("V");    
+    Serial.println(average);    
     }
-    for(leftRightPos = 180; leftRightPos > 0; leftRightPos--) {    // De derechas a izquierda
+    for(leftRightPos = 180; leftRightPos > 0; leftRightPos--) {    
     leftRightServo.write(leftRightPos);
         for (index = 0; index<numReadings; index++) {           
-        // Pulso de 10us para inicial el modulo
+        
         digitalWrite(trigPin, HIGH); 
         delayMicroseconds(10); 
         digitalWrite(trigPin, LOW);
